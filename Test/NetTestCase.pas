e28408f2d0216f796f798361a282588f697c6a28
Asbjørn Heid <lordcrc@gmail.com>
2015-05-24 16:07:58 +0200

    More CheckEquals overloads

--- Test/NetTestCase.pas
+++ Test/NetTestCase.pas
@@ -11,10 +11,15 @@ type
     procedure CheckEquals(expected, actual: IPv4Address; msg: string = ''); overload; virtual;
     procedure CheckEquals(expected, actual: IPv6Address; msg: string = ''); overload; virtual;
     procedure CheckEquals(expected, actual: IPAddress; msg: string = ''); overload; virtual;
+    procedure CheckEquals(expected, actual: IPEndpoint; msg: string = ''); overload; virtual;
+    procedure CheckEquals(expected, actual: IPProtocol; msg: string = ''); overload; virtual;
   end;
 
 
 implementation
+
+uses
+  System.SysUtils;
 
 { TNetTestCase }
 
@@ -37,6 +42,20 @@ begin
   FCheckCalled := True;
   if (expected <> actual) then
     FailNotEquals(expected, actual, msg);
+end;
+
+procedure TNetTestCase.CheckEquals(expected, actual: IPEndpoint; msg: string);
+begin
+  FCheckCalled := True;
+  if (expected <> actual) then
+    FailNotEquals(expected, actual, msg);
+end;
+
+procedure TNetTestCase.CheckEquals(expected, actual: IPProtocol; msg: string);
+begin
+  FCheckCalled := True;
+  if (expected <> actual) then
+    FailNotEquals(expected.ToString, actual.ToString, msg);
 end;
 
 end.
