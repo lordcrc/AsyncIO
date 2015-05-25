61c74de2c3529fa18e26e6b683ddcac3c7d8dfe5
Asbjørn Heid <lordcrc@gmail.com>
2015-05-26 01:05:38 +0200

    Harmonized directory names and projects
    Reference dependencies directly for now, so they're more visible.

--- dev/AsyncIODev.dpr
+++ dev/AsyncIODev.dpr
@@ -6,21 +6,26 @@ program AsyncIODev;
 
 uses
   System.SysUtils,
-  AsyncIO.ErrorCodes in '..\src\AsyncIO.ErrorCodes.pas',
-  AsyncIO in '..\src\AsyncIO.pas',
-  AsyncIO.Detail in '..\src\AsyncIO.Detail.pas',
-  AsyncIO.Filesystem in '..\src\AsyncIO.Filesystem.pas',
-  AsyncIO.Filesystem.Detail in '..\src\AsyncIO.Filesystem.Detail.pas',
-  AsyncIO.Net.IP in '..\src\AsyncIO.Net.IP.pas',
-  AsyncIO.Net.IP.Detail in '..\src\AsyncIO.Net.IP.Detail.pas',
-  AsyncIO.Net.IP.Detail.TCPImpl in '..\src\AsyncIO.Net.IP.Detail.TCPImpl.pas',
-  AsyncIO.StreamReader in '..\src\AsyncIO.StreamReader.pas',
   AsyncIO.Test.Basic in 'AsyncIO.Test.Basic.pas',
   AsyncIO.Test.Copy in 'AsyncIO.Test.Copy.pas',
   AsyncIO.Test.Socket in 'AsyncIO.Test.Socket.pas',
   AsyncIO.Test.StreamReader in 'AsyncIO.Test.StreamReader.pas',
-  AsyncIO.Detail.StreamBufferImpl in '..\src\AsyncIO.Detail.StreamBufferImpl.pas',
-  AsyncIO.Test.AsyncReadUntil in 'AsyncIO.Test.AsyncReadUntil.pas';
+  AsyncIO.Test.AsyncReadUntil in 'AsyncIO.Test.AsyncReadUntil.pas',
+  AsyncIO.Detail in '..\Source\AsyncIO.Detail.pas',
+  AsyncIO.Detail.StreamBufferImpl in '..\Source\AsyncIO.Detail.StreamBufferImpl.pas',
+  AsyncIO.ErrorCodes in '..\Source\AsyncIO.ErrorCodes.pas',
+  AsyncIO.Filesystem.Detail in '..\Source\AsyncIO.Filesystem.Detail.pas',
+  AsyncIO.Filesystem in '..\Source\AsyncIO.Filesystem.pas',
+  AsyncIO.Net.IP.Detail in '..\Source\AsyncIO.Net.IP.Detail.pas',
+  AsyncIO.Net.IP.Detail.TCPImpl in '..\Source\AsyncIO.Net.IP.Detail.TCPImpl.pas',
+  AsyncIO.Net.IP in '..\Source\AsyncIO.Net.IP.pas',
+  AsyncIO in '..\Source\AsyncIO.pas',
+  AsyncIO.StreamReader in '..\Source\AsyncIO.StreamReader.pas',
+  BufStream in '..\..\BufferedStreamReader\BufStream.pas',
+  BufStreamReader in '..\..\BufferedStreamReader\BufStreamReader.pas',
+  EncodingHelper in '..\..\BufferedStreamReader\EncodingHelper.pas',
+  RegularExpr.Detail in '..\..\RegularExpr\RegularExpr.Detail.pas',
+  RegularExpr in '..\..\RegularExpr\RegularExpr.pas';
 
 begin
   ReportMemoryLeaksOnShutdown := True;
